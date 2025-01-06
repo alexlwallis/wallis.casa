4184efc189b0d987bbafc460f661c7876f19ac82
Alex Wallis <aldwallis@gmail.com>
2025-01-06 08:47:22 +0000

    Version 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,50 @@
-import Image from "next/image";
+import Link from 'next/link';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen bg-black flex flex-col items-center justify-center p-6">
+      <div className="text-center mb-10">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <h1 className="text-4xl font-bold text-left text-white">Alex Wallis</h1>
+        <p className="text-gray-300 mt-16 max-w-2xl text-left">
+          Data Engineer with expertise in building scalable data pipelines, crafting efficient data
+          architectures, and delivering actionable insights. Passionate about blending technical
+          proficiency with strategic impact to drive data-driven decision-making.
+        </p>
+        <div className="text-left">
+          <footer className="min-h-screen flex flex-col items-left justify-left">
+            <nav className='flex space-x-6 items-left justify-left'>
+              <Link href="https://linkedin.com/in/alexlwallis" legacyBehavior>
+                <a
+                  className="flex items-center text-center text-blue-400 hover:text-blue-500 text-lg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin className="mr-2" /> LinkedIn
+                </a>
+              </Link>
+              <Link href="https://github.com/alexlwallis" legacyBehavior>
+                <a
+                  className="flex items-center text-center text-blue-400 hover:text-blue-500 text-lg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub className="mr-2" /> GitHub
+                </a>
+              </Link>
+              <Link href="mailto:awallis@cs.cmu.edu" legacyBehavior>
+                <a className="flex items-center text-center text-blue-400 hover:text-blue-500 text-lg">
+                  <FaEnvelope className="mr-2" /> Email
+                </a>
+              </Link>
+            </nav>
+          </footer>
+
+      </div>
+
+       
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
